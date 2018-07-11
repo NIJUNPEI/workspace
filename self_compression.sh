df5b4d167ea415db37c147f2a5a786371e8d977c
nijunpei <niphust@hust.edu.cn>
2018-07-12 01:40:51 +0800

    self_compression.sh

--- self_compression.sh
+++ self_compression.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 # ./self_compression.sh dir1[待解压路径] dir2[解压目标目录]
 # ./self_compression.sh --list 列出当前支持解压的文件类型
-filename=$1
-output_dir=$2
+filename="$1"
+address="$2"
 
-if [ $1="--list" ]   //判断第一个参数是不是--list，如果是则输出当前支持解压的文件类型
+#检测参数 $1 是不是 --list 注意用于判断的等号两边要有空格
+if [ "${filename}" = "--list" ]
 then 
-echo "zip tar tar.gz tar.bz2"
+   echo "zip tar tar.gz tar.bz2"
+fi
+#检测参数 $1 的结尾是什么类型的压缩文件
+# ${filename##*.}表示filename最远端的.后面的字符内容
+if [ "${filename##*.}" = "zip" ]
+then
+#检验 $2 参数是否缺省，如果缺省，则解压到当前文件夹
+   if [ ! -n "$2" ];then
+	unzip ${filename}
+   else
+	unzip ${filename} -d ${address}
+   fi
+
+elif [ "${filename##*.}" = "tar" ]
+then
+   if [ ! -n "$2" ];then
+	tar -xvf ${filename}
+   else	
+	tar -xvf ${filename} -C ${address}
+   fi
+
+elif [ "${filename##*.}" = "gz" ]
+then
+   if [ ! -n "$2" ];then
+	tar -zxvf ${filename}
+   else   
+	tar -zxvf ${filename} -C ${address}
+   fi
+elif [ "${filename##*.}" = "bz2" ]
+then
+   if [ ! -n "$2" ];then
+	tar -jxvf ${filename}
+   else
+	tar -jxvf ${filename} -C ${address}
+   fi
+
 fi
 
-if [ "${filename#*.}"="zip" ]   //判断文件后缀的类型，根据类型进行解压操作
-then
-	if [ ! -n "$2" ];then   //判断目标目录是否为空，如果为空解压到当前目录，如果不为空按路径进行解压
-	   unzip $1
-	else
-	   unzip $1 -d $2
-	fi
 
-elif [ "${filename#*.}"="tar" ]
-then
-	if [ ! -n "$2" ];then
-	   tar -xvf $1
-	else
-	   tar -xvf $1 -C $2
-	fi
 
-elif [ "${filename#*.}"="tar.gz" ]
-then
-	if [ ! -n "$2" ];then
-	   tar -zxvf $1
-	else
-	   tar -zxvf $1 -C $2
-	fi
 
-elif [ "${filename#*.}"="tar.bz2" ]
-then
-	if [ ! -n "$2" ];then
-	   tar -jxvf $1
-	else
-	   tar -jxvf $1 -C $2
-	fi
 
-fi
+
