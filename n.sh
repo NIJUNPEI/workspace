22e52c2fd3ff696f7924101e300bd02944834a4c
nijunpei <niphust@hust.edu.cn>
2018-07-11 21:58:53 +0800

    n!

---
+++ n.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#接收一个参数 n 
+#递归计算 n！打印到屏幕
+jiecheng()
+{
+   local num=$1
+   if [ $num -eq 0 ]
+   then
+      rtn=1
+   else
+      jiecheng `expr $num - 1`
+      rtn=`expr $num \* $rtn`
+   fi
+
+   return $rtn
+}
+
+jiecheng $1
+
+echo $rtn
